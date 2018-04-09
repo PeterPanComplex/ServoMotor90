d823cc7d468857ced785f5006267f35ff003c56b
DoYeon <do1217kr@gmail.com>
2018-04-09 19:20:39 +0900

    센서 수정

--- KnockOut/KnockOut.ino
+++ KnockOut/KnockOut.ino
@@ -1,3 +1,5 @@
+#include <Servo.h>
+
 /* Detects patterns of knocks and triggers a motor to unlock
    it if the pattern is correct.
    
@@ -20,10 +22,12 @@
  
 // Pin definitions
 const int knockSensor = 0;         // Piezo sensor on pin 0.
-const int programSwitch = 2;       // If this is high we program a new code.
 const int lockMotor = 3;           // Gear motor used to turn the lock.
 const int redLED = 4;              // Status LED
 const int greenLED = 5;            // Status LED
+const int servoPin = 7;
+int angle = 0; // servo position in degrees 
+
  
 // Tuning constants.  Could be made vars and hoooked to potentiometers for soft configuration, etc.
 const int threshold = 3;           // Minimum signal from the piezo to register as a knock
@@ -35,18 +39,24 @@ const int lockTurnTime = 650;      // milliseconds that we run the motor to get 
 const int maximumKnocks = 20;       // Maximum number of knocks to listen for.
 const int knockComplete = 1200;     // Longest time to wait for a knock before we assume that it's finished.
 
+Servo servo;
 
 // Variables.
-int secretCode[maximumKnocks] = {50, 25, 25, 50, 100, 50, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};  // Initial setup: "Shave and a Hair Cut, two bits."
+int secretCode1[maximumKnocks] = {50, 25, 25, 50, 100, 50, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};  // Initial setup: "Shave and a Hair Cut, two bits."
+int secretCode2[maximumKnocks] = {50, 25, 25, 50, 100, 50, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+int secretCode3[maximumKnocks] = {50, 25, 25, 50, 100, 50, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 int knockReadings[maximumKnocks];   // When someone knocks this array fills with delays between knocks.
 int knockSensorValue = 0;           // Last reading of the knock sensor.
 int programButtonPressed = false;   // Flag so we remember the programming button setting at the end of the cycle.
+
+boolean validateKnock();
 
 void setup() {
   pinMode(lockMotor, OUTPUT);
   pinMode(redLED, OUTPUT);
   pinMode(greenLED, OUTPUT);
-  pinMode(programSwitch, INPUT);
+
+  servo.attach(servoPin);
   
   Serial.begin(9600);               			// Uncomment the Serial.bla lines for debugging.
   Serial.println("Program start.");  			// but feel free to comment them out after it's working right.
@@ -56,16 +66,7 @@ void setup() {
 
 void loop() {
   // Listen for any knock at all.
-  knockSensorValue = analogRead(knockSensor);
-  
-  if (digitalRead(programSwitch)==HIGH){  // is the program button pressed?
-    programButtonPressed = true;          // Yes, so lets save that state
-    digitalWrite(redLED, HIGH);           // and turn on the red light too so we know we're programming.
-  } else {
-    programButtonPressed = false;
-    digitalWrite(redLED, LOW);
-  }
-  
+  knockSensorValue = analogRead(knockSensor);  
   if (knockSensorValue >=threshold){
     listenToSecretKnock();
   }
@@ -74,7 +75,6 @@ void loop() {
 // Records the timing of knocks.
 void listenToSecretKnock(){
   Serial.println("knock starting");   
-
   int i = 0;
   // First lets reset the listening array.
   for (i=0;i<maximumKnocks;i++){
@@ -85,15 +85,9 @@ void listenToSecretKnock(){
   int startTime=millis();           			// Reference for when this knock started.
   int now=millis();
   
-  digitalWrite(greenLED, LOW);      			// we blink the LED for a bit as a visual indicator of the knock.
-  if (programButtonPressed==true){
-     digitalWrite(redLED, LOW);                         // and the red one too if we're programming a new knock.
-  }
+  digitalWrite(greenLED, LOW);
   delay(knockFadeTime);                       	        // wait for this peak to fade before we listen to the next one.
-  digitalWrite(greenLED, HIGH);  
-  if (programButtonPressed==true){
-     digitalWrite(redLED, HIGH);                        
-  }
+  digitalWrite(greenLED, HIGH);
   do {
     //listen for the next knock or wait for it to timeout. 
     knockSensorValue = analogRead(knockSensor);
@@ -105,15 +99,9 @@ void listenToSecretKnock(){
       currentKnockNumber ++;                             //increment the counter
       startTime=now;          
       // and reset our timer for the next knock
-      digitalWrite(greenLED, LOW);  
-      if (programButtonPressed==true){
-        digitalWrite(redLED, LOW);                       // and the red one too if we're programming a new knock.
-      }
+      digitalWrite(greenLED, LOW);
       delay(knockFadeTime);                              // again, a little delay to let the knock decay.
       digitalWrite(greenLED, HIGH);
-      if (programButtonPressed==true){
-        digitalWrite(redLED, HIGH);                         
-      }
     }
 
     now=millis();
@@ -122,10 +110,10 @@ void listenToSecretKnock(){
   } while ((now-startTime < knockComplete) && (currentKnockNumber < maximumKnocks));
   
   //we've got our knock recorded, lets see if it's valid
-  if (programButtonPressed==false){             // only if we're not in progrmaing mode.
     if (validateKnock() == true){
       triggerDoorUnlock(); 
-    } else {
+    } 
+    else {
       Serial.println("Secret knock failed.");
       digitalWrite(greenLED, LOW);  		// We didn't unlock, so blink the red LED as visual feedback.
       for (i=0;i<4;i++){					
@@ -135,46 +123,31 @@ void listenToSecretKnock(){
         delay(100);
       }
       digitalWrite(greenLED, HIGH);
-    }
-  } else { // if we're in programming mode we still validate the lock, we just don't do anything with the lock
-    validateKnock();
-    // and we blink the green and red alternately to show that program is complete.
-    Serial.println("New lock stored.");
-    digitalWrite(redLED, LOW);
-    digitalWrite(greenLED, HIGH);
-    for (i=0;i<3;i++){
-      delay(100);
-      digitalWrite(redLED, HIGH);
-      digitalWrite(greenLED, LOW);
-      delay(100);
-      digitalWrite(redLED, LOW);
-      digitalWrite(greenLED, HIGH);      
-    }
-  }
+    }  
 }
 
 
 // Runs the motor (or whatever) to unlock the door.
 void triggerDoorUnlock(){
   Serial.println("Door unlocked!");
-  int i=0;
+  int i=0 ;
   
-  // turn the motor on for a bit.
-  digitalWrite(lockMotor, HIGH);
-  digitalWrite(greenLED, HIGH);            // And the green LED too.
-  
-  delay (lockTurnTime);                    // Wait a bit.
-  
-  digitalWrite(lockMotor, LOW);            // Turn the motor off.
-  
-  // Blink the green LED a few times for more visual feedback.
+  // rotate from 0 to 180 degrees
+  for(angle = 0; angle < 95; angle++) 
+  { 
+      servo.write(angle); 
+      delay(30); 
+  }
   for (i=0; i < 5; i++){   
       digitalWrite(greenLED, LOW);
-      delay(100);
+      delay(500);
       digitalWrite(greenLED, HIGH);
-      delay(100);
+      delay(500);
   }
-   
+  for(angle=95; angle >=0; angle--){    // goes from 180 degrees to 0 degrees
+    servo.write(angle);
+    delay(30); 
+  }
 }
 
 // Sees if our knock matches the secret.
@@ -185,49 +158,30 @@ boolean validateKnock(){
  
   // simplest check first: Did we get the right number of knocks?
   int currentKnockCount = 0;
-  int secretKnockCount = 0;
+  int secretKnockCount1 = 0;
+  int secretKnockCount2 = 0;
+  int secretKnockCount3 = 0;
   int maxKnockInterval = 0;          			// We use this later to normalize the times.
   
   for (i=0;i<maximumKnocks;i++){
     if (knockReadings[i] > 0){
       currentKnockCount++;
     }
-    if (secretCode[i] > 0){  					//todo: precalculate this.
-      secretKnockCount++;
+    if (secretCode1[i] > 0){  					//todo: precalculate this.
+      secretKnockCount1++;
+    }
+    if (secretCode2[i] > 0){            //todo: precalculate this.
+      secretKnockCount2++;
+    }
+    if (secretCode3[i] > 0){            //todo: precalculate this.
+      secretKnockCount3++;
     }
     
     if (knockReadings[i] > maxKnockInterval){ 	// collect normalization data while we're looping.
       maxKnockInterval = knockReadings[i];
     }
-  }
-  
-  // If we're recording a new knock, save the info and get out of here.
-  if (programButtonPressed==true){
-      for (i=0;i<maximumKnocks;i++){ // normalize the times
-        secretCode[i]= map(knockReadings[i],0, maxKnockInterval, 0, 100); 
-      }
-      // And flash the lights in the recorded pattern to let us know it's been programmed.
-      digitalWrite(greenLED, LOW);
-      digitalWrite(redLED, LOW);
-      delay(1000);
-      digitalWrite(greenLED, HIGH);
-      digitalWrite(redLED, HIGH);
-      delay(50);
-      for (i = 0; i < maximumKnocks ; i++){
-        digitalWrite(greenLED, LOW);
-        digitalWrite(redLED, LOW);  
-        // only turn it on if there's a delay
-        if (secretCode[i] > 0){                                   
-          delay( map(secretCode[i],0, 100, 0, maxKnockInterval)); // Expand the time back out to what it was.  Roughly. 
-          digitalWrite(greenLED, HIGH);
-          digitalWrite(redLED, HIGH);
-        }
-        delay(50);
-      }
-	  return false; 	// We don't unlock the door when we are recording a new knock.
-  }
-  
-  if (currentKnockCount != secretKnockCount){
+  } 
+  if (currentKnockCount != secretKnockCount1 && currentKnockCount != secretKnockCount2 && currentKnockCount != secretKnockCount3){
     return false; 
   }
   
@@ -238,19 +192,51 @@ boolean validateKnock(){
   */
   int totaltimeDifferences=0;
   int timeDiff=0;
+  int Allmismatch = 0;
+  int mismatch1 = 0;
+  int mismatch2 = 0;
+  int mismatch3 = 0;
   for (i=0;i<maximumKnocks;i++){ // Normalize the times
     knockReadings[i]= map(knockReadings[i],0, maxKnockInterval, 0, 100);      
-    timeDiff = abs(knockReadings[i]-secretCode[i]);
+    timeDiff = abs(knockReadings[i]-secretCode1[i]);
     if (timeDiff > rejectValue){ // Individual value too far out of whack
-      return false;
+      mismatch1++;
+      break;
     }
     totaltimeDifferences += timeDiff;
   }
-  // It can also fail if the whole thing is too inaccurate.
-  if (totaltimeDifferences/secretKnockCount>averageRejectValue){
-    return false; 
+  if (totaltimeDifferences/secretKnockCount1>averageRejectValue || mismatch1 == 1){
+    Allmismatch++;
   }
-  
-  return true;
-  
+  totaltimeDifferences = 0;
+  for (i=0;i<maximumKnocks;i++){ // Normalize the times
+    knockReadings[i]= map(knockReadings[i],0, maxKnockInterval, 0, 100);      
+    timeDiff = abs(knockReadings[i]-secretCode2[i]);
+    if (timeDiff > rejectValue){ // Individual value too far out of whack
+      mismatch2++;
+      break;
+    }
+    totaltimeDifferences += timeDiff;
+  }
+  if (totaltimeDifferences/secretKnockCount2>averageRejectValue || mismatch2 == 1){
+    Allmismatch++;
+  }
+  totaltimeDifferences = 0;
+  for (i=0;i<maximumKnocks;i++){ // Normalize the times
+    knockReadings[i]= map(knockReadings[i],0, maxKnockInterval, 0, 100);      
+    timeDiff = abs(knockReadings[i]-secretCode3[i]);
+    if (timeDiff > rejectValue){ // Individual value too far out of whack
+      mismatch3++;
+      break;
+    }
+    totaltimeDifferences += timeDiff;
+  }
+  if (totaltimeDifferences/secretKnockCount3>averageRejectValue || mismatch3 == 1){
+    Allmismatch++;
+  }
+  if(Allmismatch == 3)
+  {
+    return false;  
+  }
+  return true;  
 }
